ad9ed843f37760a7288962883414945490dc0cd2
chenfrank647 <chenfrank647@gmail.com>
2020-01-18 23:10:46 +0000

    Approx

--- calc/calc.c
+++ calc/calc.c
@@ -4,7 +4,7 @@
 //Testing to see if Max can push
 //test 2
 using namespace std;
-int evaluate(map<int, int> a, double x);
+double evaluate(map<int, int> a, double x);
 map<int, int> differentiate(map<int, int> a){
 	map<int, int> deriv;
 	map<int, int> :: iterator itr;
@@ -15,14 +15,14 @@ map<int, int> differentiate(map<int, int> a){
 }
 int lowerBound(map<int, int> a){
 	int test = -10;
-	while (evaluate(a, test) *evaluate(differentiate(a), test) < 0){
+	while (evaluate(a, test) * evaluate(a, test*10) < 0){
 		test = test * 10;
 	}
 	return test;
 }
 int upperBound(map<int, int> a){
 	int test = 10;
-	while (evaluate(a, test) * evaluate(differentiate(a), test) < 0){
+	while (evaluate(a, test) * evaluate(a, test) < 0){
 		test = test * 10;
 	}
 	return test;
@@ -44,31 +44,49 @@ double evaluate(map<int, int> a, double x){
 }
 
 double newton(map<int, int> a, double lower){
-	double curr = evaluate(a, lower);
 	double slope = evaluate(differentiate(a), lower);
-	double diff = curr / slope;
-	curr -= diff;
-	return curr;
+  double temp = evaluate(a, lower);
+	double diff = temp / slope;
+	lower -= diff;
+   slope = evaluate(differentiate(a), lower);
+    temp = evaluate(a, lower);
+     diff = temp / slope;
+     lower -= diff;
+     slope = evaluate(differentiate(a), lower);
+    temp = evaluate(a, lower);
+     diff = temp / slope;
+     lower -= diff;
+     slope = evaluate(differentiate(a), lower);
+    temp = evaluate(a, lower);
+     diff = temp / slope;
+     lower -= diff;
+     slope = evaluate(differentiate(a), lower);
+    temp = evaluate(a, lower);
+     diff = temp / slope;
+     lower -= diff;
+	return lower;
 }
 
 void iterate(map<int, int> a, int lower, int higher){
 	for (int i = lower; i < higher; i++){
-		if (evaluate(a, i)*evaluate(a, i+1) < 0){
+		if (evaluate(a, i)*evaluate(a, i+1) <= 0){
 			cout << newton(a, i) << endl;
+      i++;
 		}
 	}
 }
 int main(){
 	map<int, int> first;
-	first.insert(pair<int, int>(1,2));
-	first.insert(pair<int, int>(2,4));
+	first.insert(pair<int, int>(1,5));
+	first.insert(pair<int, int>(2,1));
 	map<int, int> deriv = differentiate(first);
 	map<int, int> :: iterator itr;
 	for (itr = deriv.begin(); itr != deriv.end(); itr++){
 		cout << itr -> first << endl;
 		cout << itr -> second << endl;
 	}
-	iterate(first, lowerBound(a), upperBound(a));
+  cout << lowerBound(first) << endl << upperBound(first) << endl;
+	iterate(first, lowerBound(first), upperBound(first));
 	return 0;
 
 
