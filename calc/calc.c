3611760a53956749764d71c7559c6bcf3550cd8b
chenfrank647 <chenfrank647@gmail.com>
2020-01-18 15:46:39 -0500

    derivative

--- calc/calc.c
+++ calc/calc.c
@@ -6,11 +6,26 @@ using namespace std;
 map<int, int> differentiate(map<int, int> a){
 	map<int, int> deriv;
 	map<int, int> :: iterator itr;
-	for (itr = a.begin()+1; itr != a.end(); itr++){
+	for (itr = a.begin(); itr != a.end(); itr++){
 		deriv.insert(pair<int, int>((itr->first)-1,(itr->first)*(itr->second)));
 	}
 	return deriv;
 
+}
+int power(int a, int b){
+	if (b == 0) return 1;
+	if (b == 1) return a;
+	else return  a * power(a, b-1);
+
+
+}
+int evaluate(map<int, int> a, double x){
+	int value = 0;
+	map<int, int> :: iterator itr;
+	for (itr = a.begin(); itr != a.end(); itr++){
+		value += (itr->second)*(power(x, itr->first));
+	}
+	return value;
 }
 
 int main(){
