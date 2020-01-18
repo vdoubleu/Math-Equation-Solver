4a6d59a5b7b82ca17edb0b0526afbd7019e09339
chenfrank647 <chenfrank647@gmail.com>
2020-01-18 15:24:28 -0500

    derivative

--- calc/calc.c
+++ calc/calc.c
@@ -1,11 +1,23 @@
 #include <iostream>
 #include <string>
-
+#include <map>
 using namespace std;
 
-int main(){
+map<int, int> differentiate(map<int, int> a){
+	map<int, int> deriv;
+	map<int, int> :: iterator itr;
+	for (itr = a.begin()+1; itr != a.end(); itr++){
+		deriv.insert(pair<int, int>((itr->first)-1,(itr->first)*(itr->second)));
+	}
+	return deriv;
 
-	cout << "Hello" << endl;
+}
+
+int main(){
+	map<int, int> first = { {1,4}, {2,3}  };
+	map<int, int> deriv = differentiate(first);
+
+	cout << deriv.begin()->first << endl;
 	return 0;
 
 
